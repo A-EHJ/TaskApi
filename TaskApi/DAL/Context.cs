2e67efaaeb802ecc2918a9baa71b80cbb46f0b3b
A-EHJ <Abrahamelhage2003@gmail.com>
2024-07-07 20:42:20 -0400

    Add project files.

---
+++ TaskApi/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskApi.DAL
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+       public DbSet<Models.Tasks> Tasks { get; set; }
+    }
+}
---
+++ TaskApi/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskApi.Models
+{
+    public class Tasks
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public DateTime DueDate  { get; set; }
+        public bool Completed { get; set; }
+    }
+}
